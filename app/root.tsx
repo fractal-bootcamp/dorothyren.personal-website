bd2c321983a1ab7663f42827c3c49b54801dc8ac
Steven <ssebexen@gmail.com>
2024-07-25 14:08:35 -0400

    Removed unused prop from Header.

--- app/root.tsx
+++ app/root.tsx
@@ -35,7 +35,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 export default function App() {
   return (
   <>
-    <Header selected="./" />
+    <Header />
     <Outlet />
   </>
   );
